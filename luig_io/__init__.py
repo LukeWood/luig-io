1a8871c64cb413188abc5d91539fb096a5d5c02f
Luke Wood <lukewoodcs@gmail.com>
2022-11-04 15:30:16 -0700

    Move to algorithms/ dir

--- luig_io/__init__.py
+++ luig_io/__init__.py
@@ -1,3 +1,4 @@
 import luig_io.models
 import luig_io.wrappers
 import luig_io.policy_gradient
+from luig_io.utils import set_seed
---
+++ luig_io/utils.py
@@ -0,0 +1,14 @@
+import numpy as np
+import tensorflow as tf
+import random
+
+
+def set_seed(seed):
+    """set all seeds to the value of `seed`.
+
+    Args:
+        seed: integer to be used as the seed.
+    """
+    np.random.seed(args.seed)
+    random.seed(args.seed)
+    tf.random.set_seed(args.seed)
